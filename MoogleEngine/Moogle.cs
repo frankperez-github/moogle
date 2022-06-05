65670dc33089e3d174e84f6d446c1137a1693441
Frank Perez <fp848584@gmail.com>
2022-06-04 22:20:52 -0400

    Load Texts Method created

--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -2,33 +2,38 @@
 
 public static class Moogle
 {
-    // Load Texts
+    // Load_Texts();
     // Search method(It search the frequency of each word in a text and in all texts)
     // SearchResult recieves the query and returns (SearchItem[], query)
 
-
-
-
+    
 
     public static SearchResult Query(string query) {
-        // Modifique este método para responder a la búsqueda
 
-        SearchItem[] items = new SearchItem[7] {
-            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.9f),
-            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.5f),
-            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.1f),
-            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.9f),
-            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.5f),
-            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.1f),
+        LoadTexts();
+        SearchItem[] items = new SearchItem[] {
             new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.9f),
         };
 
         return new SearchResult(items, query);
     }
-    public static bool loadTexts(){
 
-        
+    public static void LoadTexts()
+        {
+            //Addresses of txts
+            string[] filesAddress = Directory.GetFiles("../Content/", "*.txt");
+            int TxtQuant = filesAddress.Length;
 
-        return true;
-    }
+            //Matrix to storage all content of txts, each one in a string
+            string[] contentMatrix = new string[TxtQuant];
+
+            //Content of txts
+            for (int i = 0; i < TxtQuant; i++)
+            {   
+                string content = File.ReadAllText(filesAddress[i]);
+                contentMatrix[i] = content;
+            }
+        } 
 }
+
+    

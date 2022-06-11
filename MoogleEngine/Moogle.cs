221ee441cd6dc03e8170ac197f6b1f915056e423
Frank Perez <fp848584@gmail.com>
2022-06-11 18:28:34 -0400

    Making better split method

--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -1,95 +1,114 @@
 ﻿namespace MoogleEngine;
 
-public static class Moogle
+public class Moogle
 {
-
     public static SearchResult Query(string query) {
 
         LoadTexts();
-        string[] queryWords = SplitSentences(query);
+        string[] queryWords = SplitInWords(query);
 
-        //Hacer un metodo que separe el query en palabras y busque en la matriz contentMatrix
-        //Ese metodo obtiene resultados que le pasa a items(el array de aqui abajo):
-
-        SearchItem[] items = new SearchItem[] {
-
-            new SearchItem(queryWords[0], "Lorem ipsum dolor sit amet", 0.9f),
-            new SearchItem(queryWords[1], "Lorem ipsum dolor sit amet", 0.9f),
-            new SearchItem(queryWords[2], "Lorem ipsum dolor sit amet", 0.9f),
+        SearchItem[] items = new SearchItem[queryWords.Length];
+        for (var i = 0; i < queryWords.Length; i++)
+        {
+            items = new SearchItem[] {
+            new SearchItem(queryWords[i], "Lorem ipsum dolor sit amet", 0.9f)
         };
+        
+        }
 
         return new SearchResult(items, query);
     }
 
     public static void LoadTexts()
     {
-        //Addresses of txts
+        //Addresses of txts (GetFiles returns a string[])
         string[] filesAddress = Directory.GetFiles("../Content/", "*.txt");
         int TxtQuant = filesAddress.Length;
 
-        //Matrix to storage all content of txts, each one in a string
-        string[] contentMatrix = new string[TxtQuant];
+        //Dict to storage all content of txts, each text in a string[], each word as a string
+        //and as key the file address
+        Dictionary<string, string[]> TXTcontent = new Dictionary<string, string[]>();
 
-        //Content of txts
         for (int i = 0; i < TxtQuant; i++)
         {   
             string content = File.ReadAllText(filesAddress[i]).ToLower();
-            contentMatrix[i] = content;
-        }
+            TXTcontent.Add(filesAddress[i], SplitInWords(content));
+        }  
     } 
 
-          public static string[] SplitSentences(string sentence)
-        {
-            sentence = sentence.ToLower();
-            //Queda pendiente quitar espacios al final y al inicio del query
-            string[] words;
-            int wordsQuant = 0;
-            int countWords = 0;
-            int start = 0;
-            int end = 0;
+    public static string[] SplitInWords(string sentence)
+    {
+        //Normalizing text
+        sentence = sentence.ToLower();
+        sentence.Trim();
 
-            //Counting total of words
-            for(int i = 0; i < sentence.Length; i++)
+        string[] words;
+        int wordsQuant = 0;
+        int countWords = 0;
+        int start = 0;
+        int end = 0;
+
+        //Counting total of words
+        for(int i = 0; i < sentence.Length; i++)
+        {
+            if(sentence[i] == ' ' || sentence[i] == '.' || sentence[i] == ',' || sentence[i] == ';' || i == sentence.Length-1)
             {
-                if(sentence[i] == ' ' || i == sentence.Length-1)
+                wordsQuant++;
+            }
+        }
+        words = new string[wordsQuant];
+
+        
+        for(int i = 0; i < sentence.Length; i++)
+        {
+            //Spliting text in separeted words
+            if (sentence[i] == ' ' || sentence[i] == '.' || sentence[i] == ',' || 
+                sentence[i] == ';' || i == sentence.Length-1)
+            {   
+                
+                end = i;//Does not include i position beacuase sentence[i] is a white space
+
+                if(i == sentence.Length-1 && (int)(sentence[i]) >=  97 && (int)(sentence[i]) <=  122)
                 {
-                    wordsQuant++;
+                    //If last char of query is a letter, then is part of last word
+                    end = i+1;
+                }
+
+                char[] newWord = new char[end-start];
+                int tmpcount = 0;
+                
+                for(int j = start; j < end; j++)
+                {
+                    newWord[tmpcount] = sentence[j];
+                    tmpcount++;
+                }
+
+                if (countWords == wordsQuant)
+                {
+                    break;
+                }
+                words[countWords] = string.Join("", newWord);
+                countWords++;
+                //If sentence[i] is a colon,semicolon... and next char is an empty space
+                // next word start in second char from i
+                if ( i <= sentence.Length-2)
+                {
+                    if(sentence[i+1] == ' ')
+                    {
+                        start = i+2;
+                        i++;
+                    }
+                    
+                    if(!char.IsLetter(sentence[i])) //If next char is a letter next word starts in i+1
+                    {
+                        start = i+1;
+                    }
                 }
             }
-            words = new string[wordsQuant];
+        }  
 
-            //Walking trought the sentence if there is space then
-            //until there, is a word
-            for(int i = 0; i < sentence.Length; i++)
-            {
-                if (sentence[i] == ' ' || sentence[i] == '.' || sentence[i] == ',' || sentence[i] == ';' || i == sentence.Length-1)
-                {   
-                    end = i;
-                    if(i == sentence.Length-1)
-                    {
-                        end = i+1;
-                    }
-                    char[] newWord = new char[end-start];
-                    int tmpcount = 0;
-                    
-                    for(int j = start; j < end; j++)
-                    {
-                        newWord[tmpcount] = sentence[j];
-                        tmpcount++;
-                    }
-
-                    if (countWords == wordsQuant)
-                    {
-                        break;
-                    }
-                    words[countWords] = string.Join("", newWord);
-                    countWords++;
-                    start = i+1;
-                }
-            }  
-
-            return words;
-        }
+        return words;
+    }
 }
 
     
